fb1197c0166f72c17051f108e668645ed30c0bd4
alprise <cristian.prisecaru@gmail.com>
2014-01-08 12:38:12 +0100

    implemented in chat: connect, disconnect and availableusers functionality

--- src/JobMe.Web.Mvc/Hubs/ChatHub.cs
+++ src/JobMe.Web.Mvc/Hubs/ChatHub.cs
@@ -3,12 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Collections.Concurrent;
 
 namespace JobMe.Web.Mvc.Hubs
 {
     public class ChatHub : Hub
     {
-        
+        private static ConcurrentDictionary<string, string> allConnectedUsers = new ConcurrentDictionary<string, string>();
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            var currentConnectionId = this.Context.ConnectionId;
+            var datetime = DateTime.UtcNow;
+            if (this.Context.User != null)
+            {
+                var name = this.Context.User.Identity.Name;
+                allConnectedUsers.TryAdd(currentConnectionId,name);
+                var availableUsers = string.Join(",", allConnectedUsers.Where(x => x.Key != currentConnectionId).Select(y => y.Value).ToArray());
+
+                if (!string.IsNullOrEmpty(availableUsers))
+                {
+                    Clients.Caller.showAvailableUsers(availableUsers, datetime);
+                }                
+
+                Clients.AllExcept(currentConnectionId).showUserConnected(name, datetime);
+            }
+            
+            return base.OnConnected();
+        }
+        public override System.Threading.Tasks.Task OnDisconnected()
+        {
+            var currentConnectionId = this.Context.ConnectionId;
+            var datetime = DateTime.UtcNow;
+            if (this.Context.User != null)
+            {
+                var name = this.Context.User.Identity.Name;
+                string value;
+                allConnectedUsers.TryRemove(currentConnectionId, out value);
+                Clients.AllExcept(currentConnectionId).showUserDisconnected(name, datetime);
+            }
+            
+            return base.OnDisconnected();
+        }
         public void Send(string name, string message)
         {
             var datetime = DateTime.UtcNow;
